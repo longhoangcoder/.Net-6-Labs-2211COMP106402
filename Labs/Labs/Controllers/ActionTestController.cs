0ef1c88a485eff5555b26bf27a3836564ef888f5
longhoangcoder <hoanglongly942@gmail.com>
2022-09-17 15:54:43 +0700

    init

---
+++ Labs/Labs/Controllers/ActionTestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Labs.Controllers
+{
+    public class ActionTestController : Controller
+    {
+        public string Index()
+        {
+            return "HelloWorld";
+        }
+        public IActionResult ActionIndex()
+        {
+            return View("MyView");
+        }
+    }
+}
