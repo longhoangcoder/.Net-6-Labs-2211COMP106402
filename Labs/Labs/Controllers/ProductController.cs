da4b5be7f32e999209f44723df01951308b64f76
longhoangcoder <hoanglongly942@gmail.com>
2022-09-17 20:50:18 +0700

    Show All bug( line 12 )

---
+++ Labs/Labs/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Labs.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Labs.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("ShowAll");
+        }
+        public IActionResult ShowAll()
+        {
+            ViewData["Heading"] = "All Products";
+            var products = new List<Product>();
+            products.Add(new Product { ID = 101, Name = "Long", Price = 499 });
+            products.Add(new Product { ID = 102, Name = "Sang", Price = 499 });
+            products.Add(new Product { ID = 103, Name = "An", Price = 499 });
+            return View(products);
+        }
+    }
+}
