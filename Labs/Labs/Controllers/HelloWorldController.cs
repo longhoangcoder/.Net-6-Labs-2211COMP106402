ba06b7e012dfcfaaedff10d613989e34e77e8c67
longhoangcoder <hoanglongly942@gmail.com>
2022-09-17 15:37:22 +0700

    init first application

---
+++ Labs/Labs/Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Labs.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+/*
+        public string Index()
+        {
+            return "This is my default action...";
+        }*/
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+/*        public string Welcome(string name = "Thien Huong", int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }*/
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
